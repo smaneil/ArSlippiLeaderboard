41e416e6bf59d3d791cc374a5958ebabc483d4be
Costas Ford <costasford@gmail.com>
2022-12-21 21:48:12 -0800

    tag fix

--- src/components/routes/home/HomePage.tsx
+++ src/components/routes/home/HomePage.tsx
@@ -59,7 +59,7 @@ export default function HomePage() {
       <div className="p-1 text-gray-300"> Updated {updateDesc}</div>
       <Table players={players} />
       <div className="p-4 text-gray-300 flex flex-col">
-        <div>Built by blorppppp</div>
+        <div>Built by blorppppp & Modified by C4D</div>
         <div>
           <a href="https://www.buymeacoffee.com/blorppppp" target="_blank" rel="noreferrer"
              className="text-gray-400 hover:text-indigo-700 mr-2 hover:underline">
