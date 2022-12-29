9db16a81f77826a86cf4ef6c7f7087155bb1fed0
smaniel <nathanboyd97@gmail.com>
2022-12-29 13:42:54 -0600

    setup for armelee

--- cron/run.sh
+++ cron/run.sh
@@ -1,4 +1,4 @@
 #!/bin/bash -l
 DIR_PATH=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 cd "$DIR_PATH/.." 
-"/home/seefourdee/.nvm/versions/node/v18.12.0/bin/node" --loader ts-node/esm --no-warnings cron/fetchStats.ts 2>&1 | tee cron/logs/log.txt
+"C:/Users/Nathan/.nvm/versions/node/v18.12.0/bin/node.exe" --loader ts-node/esm --no-warnings cron/fetchStats.ts 2>&1 | tee cron/logs/log.txt
