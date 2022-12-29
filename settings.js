3d9f68c45adaabe1d17ec7b76672d20435dac66b
smaniel <nathanboyd97@gmail.com>
2022-12-29 14:25:29 -0600

    edited typo

--- settings.js
+++ settings.js
@@ -9,7 +9,7 @@ module.exports = {
 
   // add repo path for username.github.io/repoPath for react router to recognize paths,
   // use only when deployed to github.io, leave as null for deployments with custom domains
-  repoPath: 'https://smaniel.github.io/ArSlippiLeaderboard/',
+  repoPath: 'https://smaneil.github.io/ArSlippiLeaderboard/',
 
   // Google sheets ID for player connect codes.
   // https://docs.google.com/spreadsheets/d/[YOUR ID]
