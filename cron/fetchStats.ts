b0f659312f2b320acdcd02dcd3af1c767bede905
smaniel <nathanboyd97@gmail.com>
2022-12-31 14:25:45 -0600

    added players

--- cron/fetchStats.ts
+++ cron/fetchStats.ts
@@ -9,7 +9,7 @@ const fs = syncFs.promises;
 const execPromise = util.promisify(exec);
 
 const getPlayerConnectCodes = async (): Promise<string[]> => { 
-	return ['NATE#303','DAN#795','SOUL#906','RIMZ#185','DRU#419','RHEN#246','CYAN#654','FUCK#896','ROO#430','BRIT#1','DEMI#264','MKSC#118','CASA#299','RUDY#241'] };
+	return ['NATE#303','DAN#795','SOUL#906','RIMZ#185','DRU#419','RHEN#246','CYAN#654','FUCK#896','ROO#430','BRIT#1','DEMI#264','MKSC#118','CASA#299','RUDY#241','CREME#138','NATE#984'] };
 
 const getPlayers = async () => {
   const codes = await getPlayerConnectCodes()
