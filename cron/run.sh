0ddb9e380bbccebed84dd1480f85e69968353bd3
Daniel Perez <doperez1997@gmail.com>
2023-01-05 20:55:10 -0600

    Added a step to run.sh to create the data folder in the cron folder

--- cron/run.sh
+++ cron/run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -l
 DIR_PATH=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 cd "$DIR_PATH/.."
-mkdir data
+mkdir cron/data
 mkdir cron/logs
 touch cron/logs/log.txt
 node --loader ts-node/esm --no-warnings cron/fetchStats.ts 2>&1 | tee cron/logs/log.txt
