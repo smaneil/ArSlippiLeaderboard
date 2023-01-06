cda3aa4dce8f36593dd4a0c8532b17862c282ab2
Daniel Perez <doperez1997@gmail.com>
2023-01-05 20:58:40 -0600

    Added steps to create json files in the cron/data/ folder

--- cron/run.sh
+++ cron/run.sh
@@ -2,6 +2,8 @@
 DIR_PATH=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 cd "$DIR_PATH/.."
 mkdir cron/data
+touch cron/data/players-new.json
+touch cron/data/players-old.json
 mkdir cron/logs
 touch cron/logs/log.txt
 node --loader ts-node/esm --no-warnings cron/fetchStats.ts 2>&1 | tee cron/logs/log.txt
