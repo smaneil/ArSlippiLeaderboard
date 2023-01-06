13be325363b2a24f399007628763a8276ad239d0
Daniel Perez <doperez1997@gmail.com>
2023-01-05 21:07:08 -0600

    Added statements to show newly created directors in the console for run.sh

--- cron/run.sh
+++ cron/run.sh
@@ -6,4 +6,8 @@ touch cron/data/players-new.json
 touch cron/data/players-old.json
 mkdir cron/logs
 touch cron/logs/log.txt
+
+ls cron/data
+ls cron/logs
+
 node --loader ts-node/esm --no-warnings cron/fetchStats.ts 2>&1 | tee cron/logs/log.txt
