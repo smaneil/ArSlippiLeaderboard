02bcd7be35270a07542b3c797a012d3839b60cbc
Daniel Perez <doperez1997@gmail.com>
2023-01-05 20:35:19 -0600

    Changed the hard coded file path in run.sh

--- cron/run.sh
+++ cron/run.sh
@@ -1,4 +1,4 @@
 #!/bin/bash -l
 DIR_PATH=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
-cd "$DIR_PATH/.." 
-"C:/Users/Nathan/.nvm/versions/node/v18.12.0/bin/node.exe" --loader ts-node/esm --no-warnings cron/fetchStats.ts 2>&1 | tee cron/logs/log.txt
+cd "$DIR_PATH/.."
+node --loader ts-node/esm --no-warnings cron/fetchStats.ts 2>&1 | tee cron/logs/log.txt
