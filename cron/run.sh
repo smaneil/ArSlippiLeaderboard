7c632e3ed6b819ca51025f15c806a87994443a9d
Daniel Perez <doperez1997@gmail.com>
2023-01-05 20:49:06 -0600

    Added a step to run.sh to create the logs folder

--- cron/run.sh
+++ cron/run.sh
@@ -1,5 +1,6 @@
 #!/bin/bash -l
 DIR_PATH=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 cd "$DIR_PATH/.."
+mkdir cron/logs
 touch cron/logs/log.txt
 node --loader ts-node/esm --no-warnings cron/fetchStats.ts 2>&1 | tee cron/logs/log.txt
